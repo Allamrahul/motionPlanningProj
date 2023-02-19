2fb0fdd1e369d9174ffe4dba134d835e635e0c75
Rahul Allam <rahul.allam@danfoss.com>
2023-02-19 16:05:37 -0500

    added second map configuration

--- src/config.py
+++ src/config.py
@@ -38,8 +38,43 @@ def mapConfigs(map_config: int = 1):
                      obstacle_10,
                      obstacle_11]
 
+    elif map_config == 2:
+
+
+        # obstacle_1 = Obstacle(1, "Static", sampling_time, Circle([130, 75], 20))  # CC
+        obstacle_1 = Obstacle(1, "Static", sampling_time, Rectangle([35, 115], [30, 20]))  # LL
+        obstacle_2 = Obstacle(2, "Static", sampling_time, Rectangle([115, 115], [30, 20]))  # ML
+        obstacle_3 = Obstacle(3, "Static", sampling_time, Rectangle([195, 115], [30, 20]))  # UL
+
+        obstacle_4 = Obstacle(4, "Static", sampling_time, Rectangle([35, 65], [30, 20]))  # LL
+        obstacle_5 = Obstacle(5, "Static", sampling_time, Circle([130, 75], 13))  # ML
+        obstacle_6 = Obstacle(6, "Static", sampling_time, Rectangle([195, 65], [30, 20]))  # UL
+
+        obstacle_7 = Obstacle(7, "Static", sampling_time, Rectangle([35, 15], [30, 20]))  # LL
+        obstacle_8 = Obstacle(8, "Static", sampling_time, Rectangle([115, 15], [30, 20]))  # ML
+        obstacle_9 = Obstacle(9, "Static", sampling_time, Rectangle([195, 15], [30, 20]))  # UL
+
+        d_obs_1 = Dynamic([10.0, 100.0], 2.5, [100, 360, 0], "../Object_Photos/police.png")
+        d_obs_2 = Dynamic([240.0, 50.0], 2.5, [100, 180, 0], "../Object_Photos/taxi.png")
+        obstacle_10 = Obstacle(10, "Dynamic", sampling_time, d_obs_1)
+        obstacle_11 = Obstacle(11, "Dynamic", sampling_time, d_obs_2)
+
+        # Form a List (Do not forget to add Obstacles in this list and also add them to the Vehicles Parameters
+
+        obstacles = [obstacle_1,
+                     obstacle_2,
+                     obstacle_3,
+                     obstacle_4,
+                     obstacle_5,
+                     obstacle_6,
+                     obstacle_7,
+                     obstacle_8,
+                     obstacle_9,
+                     obstacle_10,
+                     obstacle_11]
+
 
     return obstacles
 
 
-Obstacles = mapConfigs()
+Obstacles = mapConfigs(2)
